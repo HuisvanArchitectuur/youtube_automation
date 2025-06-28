f8edbbc67c09b648d04da69ef6e6e673f689915c
HuisvanArchitectuur <Jarvin.horemans@hotmail.com>
2025-06-28 16:31:44 +0200

    Update generate_script.py

--- generate_script.py
+++ generate_script.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 import json
 import os
 
-generator = pipeline('text-generation', model='google/flan-t5-small')
+generator = pipeline('text2text-generation', model='google/flan-t5-small')
 
 with open('data/trending_topics.json') as f:
     topics = json.load(f)
@@ -16,11 +16,12 @@ prompt = (
 )
 
 results = generator(prompt, max_length=300)
+print("LLM output:", results)  # DEBUG
+
 script = results[0]['generated_text']
 
-script_file = f'data/scripts/{topic[:30].replace(" ","_")}.txt'
+script_file = f'data/scripts/{topic[:30].replace(" ", "_")}.txt'
 with open(script_file, 'w') as f:
     f.write(script)
 
 print(f"Script opgeslagen in {script_file}")
-
